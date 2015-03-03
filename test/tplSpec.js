cf0ba85be066b8b6be21717943a9ca10e0823b29
Daniel Baak <daniel.baak@gmail.com>
2015-03-03 17:38:01 +0100

    configured karma for unit testing

---
+++ test/tplSpec.js
@@ -0,0 +1,19 @@
+define(['../tpl.js'], function(tpl) {
+	describe('tpl', function() {
+		beforeEach(function() {
+			//TODO init
+		});
+
+		it('loads the template file', function() {
+			expect(false).toBe(true);
+		});
+
+		it('compiles the template', function() {
+			expect(false).toBe(true);
+		});
+
+		it('r.js precompiles the template ', function() {
+			expect(false).toBe(true);
+		});
+	});	
+});
